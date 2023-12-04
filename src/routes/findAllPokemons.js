1be8a32d6fd6a30f0e63a01a0de5f68e336fd0f2
TamaraRobson <tamara.jade.robson@gmail.com>
2023-12-04 11:35:10 +0100

    Use of sequelize operator 'Op'

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,11 +1,17 @@
-const pokemons = require("../db/mock-pokemon");
 const { Pokemon } = require("../db/sequelize");
+const { Op } = require("sequelize");
 
 module.exports = (app) => {
   app.get("/api/pokemons", (req, res) => {
     if (req.query.name) {
       const name = req.query.name;
-      return Pokemon.findAll({ where: { name: name } }).then((pokemons) => {
+      return Pokemon.findAll({
+        where: {
+          name: { //'name' est la propriété du pokémon
+            [Op.eq]: name, //'name' est le critère de la recherche
+          },
+        },
+      }).then((pokemons) => {
         const message = `Il y a ${pokemons.length} pokémons qui correspondent au terme de recherche ${name}.`;
         res.json({ message, data: pokemons });
       });
