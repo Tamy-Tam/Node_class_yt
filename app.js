3941b6fddfbbd566f467dd7fe12df05ac7dc2c42
TamaraRobson <tamara.jade.robson@gmail.com>
2023-12-05 11:53:08 +0100

    Prepare for production deployement

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require("express");
-const morgan = require("morgan");
+//const morgan = require("morgan");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const sequelize = require("./src/db/sequelize");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app
   .use(favicon(__dirname + "/favicon.ico"))
-  .use(morgan("dev"))
+  //.use(morgan("dev"))
   .use(bodyParser.json());
 
 sequelize.initDb();
+
+app.get("/", (req, res) => {
+  res.json("Hello Heroku! 👋");
+});
 
 //Here we'll have our future  endpoints
 require("./src/routes/findAllPokemons")(app);
@@ -20,7 +24,7 @@ require("./src/routes/findPokemonByPk")(app);
 require("./src/routes/createPokemon")(app);
 require("./src/routes/updatePokemon")(app);
 require("./src/routes/deletePokemon")(app);
-require("./src/routes/login")(app)
+require("./src/routes/login")(app);
 
 //Gestion d'erreur
 app.use(({ res }) => {
@@ -28,7 +32,6 @@ app.use(({ res }) => {
     "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.";
   res.status(404).json({ message });
 });
-
 
 app.listen(port, () =>
   console.log(
